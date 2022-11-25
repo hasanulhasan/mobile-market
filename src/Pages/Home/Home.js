9984981650136794b6cb112fcb61602ce7d7bed6
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-11-25 20:08:10 +0600

    statistics part added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Hero from './Hero/Hero';
+import Stat from './Stat/Stat';
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Hero></Hero>
+      <Stat></Stat>
     </div>
   );
 };
---
+++ src/Pages/Home/Stat/Stat.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Stat = () => {
+  return (
+    <div className='mt-6'>
+      <section className="p-6 dark:bg-gray-800 dark:text-gray-100">
+        <h1 className='text-5xl font-extrabold p-2 text-center py-4'>Our Statistics </h1>
+        <div className="container mx-auto grid justify-center grid-cols-2 text-center lg:grid-cols-3">
+          <div className="flex flex-col justify-start m-2 lg:m-6">
+            <p className="text-4xl font-bold leading-none lg:text-6xl">5000+</p>
+            <p className="text-sm sm:text-base">Regular User</p>
+          </div>
+          <div className="flex flex-col justify-start m-2 lg:m-6">
+            <p className="text-4xl font-bold leading-none lg:text-6xl">89K</p>
+            <p className="text-sm sm:text-base">Followers on social media</p>
+          </div>
+          <div className="flex flex-col justify-start m-2 lg:m-6">
+            <p className="text-4xl font-bold leading-none lg:text-6xl">400+</p>
+            <p className="text-sm sm:text-base">Regular Seller</p>
+          </div>
+          <div className="flex flex-col justify-start m-2 lg:m-6">
+            <p className="text-4xl font-bold leading-none lg:text-6xl">12+</p>
+            <p className="text-sm sm:text-base">Media Partner</p>
+          </div>
+          <div className="flex flex-col justify-start m-2 lg:m-6">
+            <p className="text-4xl font-bold leading-none lg:text-6xl">100K</p>
+            <p className="text-sm sm:text-base">Mobile exchange</p>
+          </div>
+          <div className="flex flex-col justify-start m-2 lg:m-6">
+            <p className="text-4xl font-bold leading-none lg:text-6xl">100+</p>
+            <p className="text-sm sm:text-base">Agents</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Stat;
